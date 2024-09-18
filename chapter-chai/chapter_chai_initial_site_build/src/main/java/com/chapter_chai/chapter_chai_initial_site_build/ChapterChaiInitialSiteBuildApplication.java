bd2aa9fe9d15004dfec7824d72b49070491c7df3
StrawberryNougat <eleanormather02@gmail.com>
2024-09-17 22:27:45 -0400

    initial set up to switch to springboard project

---
+++ chapter-chai/chapter_chai_initial_site_build/src/main/java/com/chapter_chai/chapter_chai_initial_site_build/ChapterChaiInitialSiteBuildApplication.java
@@ -0,0 +1,13 @@
+package com.chapter_chai.chapter_chai_initial_site_build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapterChaiInitialSiteBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapterChaiInitialSiteBuildApplication.class, args);
+	}
+
+}
---
+++ chapter-chai/chapter_chai_initial_site_build/src/test/java/com/chapter_chai/chapter_chai_initial_site_build/ChapterChaiInitialSiteBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chapter_chai.chapter_chai_initial_site_build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapterChaiInitialSiteBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
