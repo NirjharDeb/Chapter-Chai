f95d8dee5aa2f4b299188b4f6dac582baebfaa69
George, Daniel Kyle <80804355+danny-george@users.noreply.github.com>
2024-09-26 18:43:14 -0400

    added sign in page
    doesnt include create account yet

---
+++ chapter_chai/frontend/src/sign-in-page/SignInPage.js
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './SignInPage.css';
+
+function SignInPage() {
+
+    const [sign_in, setSignIn] = useState(true); // toggle for sign-in/create account
+    // TODO: toggle button and link values based on sign_in
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = () => {
+        // (console logs used in debugging for now)
+
+        // TODO: take username and password and send to backend?
+        console.log("username:", username)
+        console.log("password:", password)
+
+        // TODO: also send whether its sign-in or create account?
+        if (sign_in) {
+            console.log("signing in");
+        } else {
+            console.log("create new account");
+        }
+    };
+
+    return (
+        <div className="sign-in-page">
+            <div className='sign-in-content'>
+                <div id="title-card">
+                    <h1 id="title">Chapter & Chai</h1>
+                    <p id="slogan">Discover, Read, Relax, Repeat</p>
+                </div>
+                <div className="form">
+                    <div className='enter-fields'>
+                        <p className='enter-text'>Username</p>
+                        <input id='username-field'
+                            placeholder='Username'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className='enter-fields'>
+                        <p className='enter-text'>Password</p>
+                        <input id='password-field'
+                            placeholder='Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button id='submit-button' onClick={handleSubmit}>Sign In</button>
+                    <div>
+                        <a href="https://google.com">New User?</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SignInPage;
