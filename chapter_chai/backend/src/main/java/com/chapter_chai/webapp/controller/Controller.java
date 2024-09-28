0f20635674ab20494125ea4310fd0a35f0cc9754
StrawberryNougat <eleanormather02@gmail.com>
2024-09-27 20:16:39 -0400

    Added initial unit tests

--- chapter_chai/backend/src/main/java/com/chapter_chai/webapp/controller/Controller.java
+++ chapter_chai/backend/src/main/java/com/chapter_chai/webapp/controller/Controller.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 import java.security.Principal;
-
+// import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.context.annotation.Bean;
 
 
 
--- chapter_chai/backend/src/test/java/com/chapter_chai/webapp/WebappApplicationTests.java
+++ chapter_chai/backend/src/test/java/com/chapter_chai/webapp/WebappApplicationTests.java
@@ -1,13 +1,39 @@
 package com.chapter_chai.webapp;
+import java.security.Principal;
+import com.chapter_chai.webapp.controller.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
 class WebappApplicationTests {
+	@Autowired
+	private Controller controller;
+	@Autowired
+	private WebappApplication application;
+	@Autowired
+	private SpringConfig springConfig;
+
+
+
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+		assertThat(application).isNotNull();
+		assertThat(springConfig).isNotNull();
 	}
 
+	
 }
