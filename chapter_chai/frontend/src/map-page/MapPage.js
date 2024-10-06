3cc7d9a56ba64ff4a6eab7dc435d1fda92a503ca
Abou El Maali, Mariam <mabouelmaali@gatech.edu>
2024-10-06 12:04:57 -0400

    had to remove the springboot application stuff and google maps API key

--- chapter_chai/frontend/src/map-page/MapPage.js
+++ chapter_chai/frontend/src/map-page/MapPage.js
@@ -122,8 +122,7 @@ function MapPage() {
     return (
         <>
             <LoadScript
-                //googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
-                googleMapsApiKey={"AIzaSyAQzSw091TkcMWpTUrwP54WJH2jN-6pzKo"}
+                googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
                 libraries={libraries}
             >
 
